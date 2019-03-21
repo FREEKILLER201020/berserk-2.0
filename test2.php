f0a8af6cab25e6b0ee034dc72d3a690b76668a0f
freekiller201020 <htc.burchenko@gmail.com>
2019-03-21 14:19:34 +0300

    data scaning test 3

--- test2.php
+++ test2.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <a id="foo" href="introduction.php">Home<span><img src="littleegret.jpg" alt="Little Egret"></span></a>
-  </body>
-  <script>
-  $('#foo').hover(
-function(){
-alert('Вы попали на территорию элемента "foo", известную своей валидной версткой'+
-      'и наличием диких обработчиков событий.');
-},
-function(){
-alert('Вы покинули территорию элемента "foo". Мы будем рады видеть вас снова.');
-});
-  </script>
-</html>
+<?php
+require("class.php");
+require("functions.php");
+
+$file  = file_get_contents(realpath(dirname(__FILE__))."/../config.json");
+$config = json_decode($file, true);
+$connection=Connect($config);
+print_r($connection);
